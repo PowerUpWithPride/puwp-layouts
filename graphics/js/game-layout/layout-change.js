62c618b9f19144dc070661463d9af1106732ad6a
DorkmasterFlek <dorkmasterflek@gmail.com>
2020-05-16 14:26:43 -0400

    Fix font size for game name after changing layout

--- graphics/js/game-layout/layout-change.js
+++ graphics/js/game-layout/layout-change.js
@@ -30,15 +30,9 @@ $(() => {
     }
 });
 
-// Update the current layout by doing the following:
-// 1. Hide all layout containers.
-// 2. Update the applied CSS file for the new layout.
-// 3. Show the layout container for the new layout.
-// 4. Check to fix pronoun wrapping.
+// Update the current layout by switching the CSS file.
 function changeLayout(layoutInfo) {
-    //$(".game-layout-container").hide();
     let cssURL = 'css/game-layout/' + layoutInfo.code + '.css';
     $('#layout-css-file').attr('href', cssURL);
-    //$("#layout-container-" + layoutInfo.code).show();
-    // fixPronounWrapping(layoutInfo);
+    FixSize('#game-name');
 }
